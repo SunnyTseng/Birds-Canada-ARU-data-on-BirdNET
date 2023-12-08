c3435db2a9ddd25216dd434d07cd2a442fdfd0a4
Sunny Tseng <sunnyyctseng@gmail.com>
2023-12-07 17:36:29 -0800

    SM4 date extraction unfinished

--- R/1_data_cleaning.R
+++ R/1_data_cleaning.R
@@ -6,9 +6,9 @@ library(here)
 
 # import data  ------------------------------------------------------------
 site <- "AxeLake"
-location <- 1
-aru_id <- 9126
-type <- "BARLT"
+location <- 2
+aru_id <- 7859
+type <- "Mini"
 site_folder <- paste(site, location, aru_id, type, sep = "_")
 
 site_data <- list.files(path = here("data", "raw", site_folder), 
@@ -26,6 +26,7 @@ site_data <- list.files(path = here("data", "raw", site_folder),
   bind_rows()
 
 
+
 # data wrangling ----------------------------------------------------------
 site_data_clean <- site_data %>%
   mutate(site = site,
@@ -35,6 +36,17 @@ site_data_clean <- site_data %>%
          date = str_extract(filepath, pattern = "\\d{8}(?=T)"), 
          hour = str_extract(filepath, pattern = "(?<=T)\\d{2}")) %>%
   select(site, location, aru_id, type, date, hour, start, end, scientific_name, common_name, confidence, filepath)
+
+
+# site_data_clean <- site_data %>%
+#   mutate(site = site,
+#          location = location,
+#          aru_id = aru_id,
+#          type = type,
+#          date = str_extract(filepath, pattern = "\\d{8}(?=T)"), 
+#          hour = str_extract(filepath, pattern = "(?<=SMA)\\d{2}")) %>%
+#   select(site, location, aru_id, type, date, hour, start, end, scientific_name, common_name, confidence, filepath)
+#   
   
 
 # saving the final file ---------------------------------------------------
