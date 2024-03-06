690eb98812e5d708c1fd63529170a27116df5242
Sunny Tseng <sunnyyctseng@gmail.com>
2024-03-06 10:08:53 -0800

    created single csv file for summary

---
+++ R/2_focal_summary_as_single_csvfile.R
@@ -0,0 +1,18 @@
+
+# library -----------------------------------------------------------------
+
+library(tidyverse)
+library(here)
+
+
+# main code here ----------------------------------------------------------
+
+site_data <- list.files(path = here("data", "cleaned", "focal"), 
+                        pattern = ".csv",
+                        full.names = TRUE,
+                        recursive = TRUE) %>%
+  .[!grepl("random", .)] %>%
+  lapply(FUN = read_csv) %>%
+  bind_rows()
+
+write_csv(site_data, here("data", "cleaned", "focal_summary_all.csv"))
